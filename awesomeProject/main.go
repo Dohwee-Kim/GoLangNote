1b116a75ec2a34c2acdbcb5b7f1a7ee1a0e304c4
Leo <cloudkim2024@gmail.com>
2025-02-07 22:11:12 -0500

    GoLang Helloworld

---
+++ awesomeProject/main.go
@@ -0,0 +1,11 @@
+package main
+
+// All Go files start with package
+// Defines the namespace for the code in the file, used to organize and group code
+// main package is special, because it indicates that the file belongs to an executable program
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello Go World")
+}
